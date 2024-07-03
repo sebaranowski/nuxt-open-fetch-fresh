afd3efe3089e2a064a5dc9737b6bbfd2c7680cc4
Sebastian Baranowski <baranowski@silpion.de>
2024-07-03 18:53:52 +0200

    add nuxt-open-fetch module with example fetch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "nuxt-open-fetch"],
   compatibilityDate: "2024-07-03",
   eslint: {
     // options here
+  },
+  openFetch: {
+    clients: {
+      // pets: {},
+      skilldb: {},
+    }
+  },
+  runtimeConfig: {
+    openFetch: {
+      // pets: {
+      //   schema: 'https://petstore3.swagger.io/api/v3/openapi.json',
+      //   baseURL: 'https://petstore3.swagger.io/api/v3/'
+      // },
+      skilldb: {
+        schema: 'http://localhost:1337/openapi.json',
+        baseURL: 'http://localhost:1337/',
+      },
+    },
   },
   devtools: { enabled: true },
 })
--- pages/nuxt-open-fetch.vue
+++ pages/nuxt-open-fetch.vue
@@ -2,8 +2,32 @@
 const title = 'Nuxt Open Fetch';
 
 console.log('foo');
+
+const { data: userWithId2 } = await useLazySkilldb('/users/{id}', {
+  path: {
+    id: '2',
+  },
+});
+
+console.log(`userWithId2:`, userWithId2);
+
+// const { data: petWithId2 } = await usePets('/pet/{petId}', {
+//   path: {
+//     petId: 2,
+//   },
+// });
+
+// console.log(`petWithId2:`, petWithId2);
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <p>userWithId2:</p>
+    <pre>{{ userWithId2 }}</pre>
+
+    <!-- <p>petWithId2:</p>
+    <pre>{{ petWithId2 }}</pre> -->
+  </div>
 </template>
